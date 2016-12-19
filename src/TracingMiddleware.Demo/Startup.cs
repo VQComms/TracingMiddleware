b9cc207ff1ec04e1510bc64cd297b6d8c7f6e4b7
Jonathan Channon <jonathan.channon@gmail.com>
2016-12-19 12:18:05 +0000

    tidied startup

--- src/TracingMiddleware.Demo/Startup.cs
+++ src/TracingMiddleware.Demo/Startup.cs
@@ -15,41 +15,47 @@
         {
             //You can use defaultoptions
             var defaultOptions = TracingMiddlewareOptions.Default;
-            
+
             //You can use custom options
-            Func<HttpContext, bool> internalexceptionfilter = context =>
-            {
-                return context.Response.StatusCode == 500 || context.Response.StatusCode == 404;
-            };
+            Func<HttpContext, bool> internalexceptionfilter = context => { return context.Response.StatusCode == 500 || context.Response.StatusCode == 404; };
 
             var filters = new[] { internalexceptionfilter };
 
             var otheroptions =
                 new TracingMiddlewareOptions(
-                    TracingMiddlewareOptions.DefaultTrace, //Console.WriteLine or overwrite with own trace handler
-                    MessageFormat,
-                    TracingMiddlewareOptions.DefaultTypeFormat, //object.ToString()
-                    filters)
-                    .ForType<IDictionary<string, string[]>>(
+                        TracingMiddlewareOptions.DefaultTrace, //Console.WriteLine or overwrite with own trace handler
+                        MessageFormat,
+                        TracingMiddlewareOptions.DefaultTypeFormat, //object.ToString()
+                        filters)
+                    .ForType<Microsoft.AspNetCore.Server.Kestrel.Internal.Http.FrameRequestHeaders>(
                         headers => string.Join(",",
                             headers.Select(
-                                header => string.Format("[{0}:{1}]", header.Key, string.Join(",", header.Value))))) //Make nice with OWIN headers
-                    .ForKey("owin.ResponseStatusCode", (requestId,value) => Console.WriteLine(requestId + " : *****" + value + "*****")) //Display status code differently
-                    .Ignore<Stream>() //Ignore OWIN keys that are Stream types
-                    .Ignore(key => key.StartsWith("")) //Ignore blank keys
-                    .ForType<ClaimsPrincipal>(user=>string.Join(",", user.Claims.Select(x=>x.Type + ":" + x.Value)));
-                    //.Include(key => key.StartsWith("owin.")); //Trace only keys that start with OWIN
+                                header => string.Format("[{0}:{1}]", header.Key, string.Join(",", header.Value))))) //Make nice with request headers
+                    .ForType<Microsoft.AspNetCore.Server.Kestrel.Internal.Http.FrameResponseHeaders>(
+                        headers => string.Join(",",
+                            headers.Select(
+                                header => string.Format("[{0}:{1}]", header.Key, string.Join(",", header.Value))))) //Make nice with response headers
+                    .ForType<Microsoft.AspNetCore.Http.Internal.RequestCookieCollection>(
+                        headers => string.Join(",",
+                            headers.Select(
+                                header => string.Format("[{0}:{1}]", header.Key, string.Join(",", header.Value))))) //Make nice with cookies
+                    .ForType<ClaimsPrincipal>(user => string.Join(",", user.Claims.Select(x => x.Type + ":" + x.Value))) //Show claims
+                    .ForKey("owin.ResponseStatusCode", (requestId, value) => Console.WriteLine(requestId + " : *****" + value + "*****")) //Display status code differently
+                    .Ignore<Stream>() //Ignore keys that are Stream types
+                    .Ignore(key => key.StartsWith("plop")); //Ignore keys that start with plop
 
             var alt = TracingMiddlewareOptions.Default.AddFilter(internalexceptionfilter);
 
             app.UseTracingMiddleware(otheroptions);
 
-            //Pass to your App
-            app.UseOwin(x =>
+            app.Use(async (context, next) =>
             {
-
-                x.UseNancy();
+                context.User.AddIdentity(new ClaimsIdentity(new List<Claim>() { new Claim("Name", "FRED") }));
+                await next();
             });
+
+            //Pass to your App
+            app.UseOwin(x => { x.UseNancy(); });
         }
 
         private string MessageFormat(string key, string value)
